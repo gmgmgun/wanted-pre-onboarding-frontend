affceef2d0a5c1d70bf524787ac3566fa3902da1
dongminlee <gmgmgun@gmail.com>
2023-04-03 16:54:41 +0900

    Add: SignUp 컴포넌트 추가
    Add: data-testid 속성
    Add: fecth 후 repsonse에 따른 분기별 처리
    Remove: StyledFormTitle
    Fix: inputValueList, setInputValueList -> userInputList, setUserInputList
    Fix: 버튼 활성화 조건
    
    Fix: Fetch 호출 방식 (Button OnClick -> Form onSubmit)
    Fix: BASE_URL
    Fix: userId Input type (email -> text)

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,234 @@
-import React from "react";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {BASE_URL} from "../config";
+import styled from "styled-components";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [userInputList, setUserInputList] = useState({
+    userId: "",
+    userPw: "",
+  });
+
+  const {userId, userPw} = userInputList;
+
+  const navigate = useNavigate();
+
+  const idRegEx = /.*@.*/;
+  const pwRegEx = /.{8,}/;
+
+  const idCheck = idRegEx.test(userId);
+  const pwCheck = pwRegEx.test(userPw);
+
+  const isValid = idCheck && pwCheck;
+
+  const printInfoText = (name, value) => {
+    if (!value) return false;
+
+    switch (name) {
+      case "userId":
+        if (!idRegEx.test(value)) {
+          return <StyledInfoText>아이디에 @를 포함시켜 주세요.</StyledInfoText>;
+        } else {
+          return <StyledInfoText>조건을 충족했습니다!</StyledInfoText>;
+        }
+
+      case "userPw":
+        if (!pwRegEx.test(value)) {
+          return (
+            <StyledInfoText>비밀번호는 8글자 이상이어야 합니다.</StyledInfoText>
+          );
+        } else {
+          return <StyledInfoText>조건을 충족했습니다!</StyledInfoText>;
+        }
+
+      default:
+    }
+  };
+
+  const onChangeInfo = ({target}) => {
+    const {name, value} = target;
+    setUserInputList({...userInputList, [name]: value});
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    fetch(`${BASE_URL}/auth/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        email: userId,
+        password: userPw,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          navigate("/signin");
+        } else {
+          alert("이미 가입된 이메일입니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("에러가 발생했습니다. 다시 시도해주세요.");
+      });
+  };
+
+  return (
+    <StyledSignUpWrapper>
+      <StyledTitle>회원가입</StyledTitle>
+      <StyledForm onSubmit={onSubmitForm}>
+        <StyledInputList>
+          {SIGNUP_INPUT_DATA.map((input) => {
+            const {id, name, type, placeholder, autoFocus, infoText} = input;
+            return (
+              <StyledInputWrap key={id}>
+                <StyledInput
+                  data-testid={
+                    name === "userId" ? "email-input" : "password-input"
+                  }
+                  onChange={onChangeInfo}
+                  placeholder=" "
+                  name={name}
+                  type={type}
+                  autoFocus={autoFocus}
+                  value={userInputList[name] || ""}
+                />
+                <StyledInputLabel>{placeholder}</StyledInputLabel>
+                {printInfoText(name, userInputList[name]) ? (
+                  printInfoText(name, userInputList[name])
+                ) : (
+                  <StyledInfoText>{infoText}</StyledInfoText>
+                )}
+              </StyledInputWrap>
+            );
+          })}
+        </StyledInputList>
+        <StyledButton
+          data-testid="signup-button"
+          type="submit"
+          disabled={!isValid}
+        >
+          가입하기
+        </StyledButton>
+      </StyledForm>
+    </StyledSignUpWrapper>
+  );
 };
 
 export default SignUp;
+
+const StyledSignUpWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 100px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 40px;
+`;
+
+const StyledForm = styled.form`
+  margin-top: 50px;
+`;
+
+const StyledInfoText = styled.p`
+  margin: 5px 0px 0px 3px;
+  opacity: 0.6;
+  font-size: 11px;
+`;
+
+const StyledInputList = styled.ul``;
+
+const StyledInputWrap = styled.li`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  margin-top: 1px;
+  padding-top: 13px;
+`;
+
+const StyledInput = styled.input`
+  min-width: 300px;
+  height: 45px;
+  padding: 0px 15px;
+  -webkit-appearance: none;
+  -webkit-transition: all 0.1s linear;
+  -moz-transition: all 0.1s linear;
+  transition: all 0.1s linear;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  outline: none;
+  font-size: 16px;
+  font-weight: 400;
+
+  &:focus {
+    padding-top: 20px;
+    border: 2px solid #d392ff;
+    font-size: 15px;
+  }
+
+  &:not(:placeholder-shown) {
+    padding-top: 20px;
+  }
+`;
+
+const StyledInputLabel = styled.label`
+  position: absolute;
+  top: calc(50% - 5px);
+  left: 15px;
+  box-sizing: border-box;
+  transition: all 0.1s linear;
+  background-color: white;
+  color: lightgray;
+  font-size: 13px;
+  pointer-events: none;
+
+  &.input-vacant {
+    top: calc(50% - 5px);
+  }
+
+  input:focus + &,
+  input:not(:placeholder-shown) + & {
+    top: 20px;
+    color: rgb(200, 200, 200);
+    font-size: 10px;
+  }
+`;
+
+const StyledButton = styled.button`
+  min-width: 300px;
+  height: 45px;
+  margin-top: 20px;
+  border: 0;
+  border-radius: 5px;
+  background-color: #d392ff;
+  color: white;
+
+  &:disabled {
+    background-color: rgb(230, 230, 230);
+    color: lightgray;
+  }
+`;
+
+const SIGNUP_INPUT_DATA = [
+  {
+    id: 0,
+    name: "userId",
+    type: "text",
+    placeholder: "아이디",
+    infoText: "아이디를 입력해주세요.",
+    autoFocus: true,
+  },
+  {
+    id: 1,
+    name: "userPw",
+    type: "password",
+    placeholder: "비밀번호",
+    infoText: "비밀번호를 입력해주세요.",
+    autoFocus: false,
+  },
+];
