14f9d84c1725f771deb06848d8f9a7a3ea9a7a11
dongminlee <gmgmgun@gmail.com>
2023-04-05 12:19:46 +0900

    Add: styles/mixin.js

---
+++ src/styles/mixin.js
@@ -0,0 +1,9 @@
+import {css} from "styled-components";
+
+export const pageWrapperMixin = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 100px;
+`;
